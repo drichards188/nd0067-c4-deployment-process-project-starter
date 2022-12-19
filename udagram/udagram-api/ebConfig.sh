9874b7b73cf56e3904c31b5801b4316fa46e269e
dRichards188 <github@cxoprotect.com>
2022-12-19 15:16:13 -0700

    testing config file for eb

---
+++ udagram/udagram-api/ebConfig.sh
@@ -0,0 +1,8 @@
+eb setenv POSTGRES_USERNAME=postgres
+eb setenv POSTGRES_PASSWORD=myPassword
+eb setenv POSTGRES_HOST=udacitydb-1.cpzbjiftntyh.us-east-1.rds.amazonaws.com
+eb setenv POSTGRES_DB=postgres
+eb setenv AWS_BUCKET=arn:aws:s3:::mybucket121422
+eb setenv AWS_REGION=us-east-1
+eb setenv AWS_PROFILE=default
+eb setenv JWT_SECRET=mysecretstring
