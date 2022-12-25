a44a723931be3c2649b94a8e73cd1e1178dcad5a
david <atlassian1832@cxoprotect.com>
2022-12-24 18:31:46 -0700

    working locally

---
+++ udagram/set_env.sh
@@ -0,0 +1,8 @@
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=myPassword
+export POSTGRES_HOST=udagram-db.ctmemwqudyt5.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagram-bucket122022
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
